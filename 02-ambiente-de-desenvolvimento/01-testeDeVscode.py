85a8be3fbd956854960c02dbe78a2829e1d38578
Jorge Harbes <jharbes@hotmail.com>
2023-03-02 21:13:52 -0300

    Initial commit

---
+++ 02-ambiente-de-desenvolvimento/01-testeDeVscode.py
@@ -0,0 +1,5 @@
+print('oi')
+print('oi')
+print('oi') # shift+alt+up copia linha pra cima,  shift+alt+down copia linha pra baixo
+print('oi')
+print('oi')
---
+++ README.md
@@ -0,0 +1,2 @@
+# python3
+ Python 3 com PySide6, Django, Selenium, Regexp, Testes, TDD, POO, Design Patterns GoF
