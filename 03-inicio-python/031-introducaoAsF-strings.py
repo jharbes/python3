34f8db89024376d2bcb7e3a8fc50a6ca1658fd16
Jorge Harbes <jharbes@hotmail.com>
2023-03-03 16:52:25 -0300

    Update 031-introducaoAsF-strings.py

--- 03-inicio-python/031-introducaoAsF-strings.py
+++ 03-inicio-python/031-introducaoAsF-strings.py
@@ -1,12 +1,13 @@
 nome = 'Luiz Otávio'
 altura = 1.80
 peso = 95
+mochila = 20
 imc = peso / altura ** 2
 
 "f-strings"
 linha_1 = f'{nome} tem {altura:.2f} de altura,'
-linha_2 = f'pesa {peso} quilos e seu imc é'
-linha_3 = f'{imc:.2f}'
+linha_2 = f'pesa {peso + mochila} quilos e seu imc é' # permite operacoes aritmeticas
+linha_3 = f'{imc:.2f}' # mantem apenas duas casas decimais
 
 print(linha_1)
 print(linha_2)
@@ -14,4 +15,4 @@ print(linha_3)
 
 # Luiz Otávio tem 1.80 de altura,
 # pesa 95 quilos e seu IMC é
-# 29.320987654320987
+# 29.32
