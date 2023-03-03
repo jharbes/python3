cb4091732947b00599efc16fdf4f62d9663487e8
Jorge Harbes <jharbes@hotmail.com>
2023-03-03 15:45:28 -0300

    Update 027-concatenacaoERepeticaoComAritmeticos.py

--- 03-inicio-python/027-concatenacaoERepeticaoComAritmeticos.py
+++ 03-inicio-python/027-concatenacaoERepeticaoComAritmeticos.py
@@ -1,6 +1,10 @@
+# concatena as strings
 concatenacao = 'Luiz' + ' ' + 'Otávio'
+concatenacao2 = 'Luiz' + str(12) + 'Otávio'
 print(concatenacao)
+print(concatenacao2)
 
+# 'multiplica' as strings (concatena n vezes)
 a_dez_vezes = 'A' * 10
 tres_vezes_luiz = 3 * 'Luiz'
 print(a_dez_vezes)
