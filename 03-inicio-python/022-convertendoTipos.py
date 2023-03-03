03fc40e1fb19c2c310927146384cedeaa7a25061
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-03 13:54:50 -0300

    Update 022-convertendoTipos.py

--- 03-inicio-python/022-convertendoTipos.py
+++ 03-inicio-python/022-convertendoTipos.py
@@ -5,12 +5,12 @@
 # str, int, float, bool
 
 
-print(int('1'), type(int('1'))) 
-print(type(float('1') + 1)) # float
-print(" ",bool(' ')) # True
-print('',bool('')) # False
-print(1,bool(1)) # True
-print(0,bool(0)) # False
-print(2,bool(2)) # True
-print(1,bool(1)) # True
+print(int('1'), type(int('1'))) # 1 int
+print(float('1')+1, type(float('1') + 1)) # float
+print(" ", bool(' ')) # True
+print('', bool('')) # False
+print(1, bool(1)) # True
+print(0, bool(0)) # False
+print(2, bool(2)) # True
+print(1, bool(1)) # True
 print(str(11) + 'b') # 11b
