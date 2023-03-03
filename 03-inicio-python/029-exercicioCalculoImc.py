402aa327150fd75298a719adc37afe611755527e
Jorge Harbes <jharbes@hotmail.com>
2023-03-03 16:13:40 -0300

    Update 029-exercicioCalculoImc.py

--- 03-inicio-python/029-exercicioCalculoImc.py
+++ 03-inicio-python/029-exercicioCalculoImc.py
@@ -3,4 +3,8 @@ altura=1.82
 peso=86
 imc=peso/altura**2
 
-print(nome + ' tem ' + str(altura) + ' de altura, pesa ' + str(peso) + ' quilos e seu IMC é ' + str(imc))
+print(nome + ' tem ' + str(altura) + ' de altura,\npesa ' + str(peso) + ' quilos e seu IMC é\n' + str(imc))
+
+# placeholder sao os ... , ele nao da erro no codigo, normalmente usado para indicar um codigo ainda a ser escrito
+print(...)
+placeholder=...
