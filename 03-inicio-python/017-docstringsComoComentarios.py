7c23dbfe9527e1950ba114bd3f1be5e27221ed7f
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-03 10:49:03 -0300

    organizacao projeto toolbar

---
+++ 03-inicio-python/017-docstringsComoComentarios.py
@@ -0,0 +1,17 @@
+"""
+DocString
+E escrever o que eu
+quiser
+asdfasdfd
+"""
+
+''' 
+Usar para escrever suas notas 
+'''
+
+# os docstrings funcionam tanto com aspas simples e aspas duplas, docstrings NAO sao comentarios
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
