221454afee10a38077f8ce6d8f7a49b1acffe3e6
Jorge Harbes <jharbes@hotmail.com>
2023-03-03 17:19:48 -0300

    Update 032-formatandoStringsMetodoFormat.py

--- 03-inicio-python/032-formatandoStringsMetodoFormat.py
+++ 03-inicio-python/032-formatandoStringsMetodoFormat.py
@@ -1,7 +1,13 @@
 a = 'AAAAA'
 b = 'BBBBBB'
 c = 1.1
+
+string0 = 'a={} b={} c={}'
 string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+
+print(string0.format(a,b,c))
+print(string)
+
 formato = string.format(
     nome1=a, nome2=b, nome3=c
 )
