1012b40d936559a979a03604637b9decbf162c81
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-03 14:24:15 -0300

    Create 024-exercicioTipagemEVariaveis.py

---
+++ 03-inicio-python/024-exercicioTipagemEVariaveis.py
@@ -0,0 +1,13 @@
+nome='Jorge'
+sobrenome='Nami Harbes'
+idade=39
+ano_nascimento=1983
+maior_de_idade=39>=18
+altura_metros=1.82
+
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('É maior de idade?',maior_de_idade)
+print('Altura em metros:',altura_metros)
