a66ce6825c69b725c26222f9afc9793fe887abf7
Jorge Harbes <jharbes@hotmail.com>
2023-03-04 17:57:11 -0300

    interpolacao de string com %

--- 03-inicio-python/044-interpolacaoStringPythonCom%.py
+++ 03-inicio-python/044-interpolacaoStringPythonCom%.py
@@ -1,12 +1,15 @@
 """
-Interpolação básica de strings
-s - string
-d e i - int
-f - float
-x e X - Hexadecimal (ABCDEF0123456789)
+Interpolação básica de strings (herança de C)
+%s - string
+%d e %i - int
+%f - float
+%x (com letras minusculas no hexadecimal) e %X (com letras maiusculas no hexadecimal) - Hexadecimal (ABCDEF0123456789)
+%06X - seis digitos hexadecimal com minusculas
 """
+
 nome = 'Luiz'
 preco = 1000.95897643
 variavel = '%s, o preço é R$%.2f' % (nome, preco)
 print(variavel)
 print('O hexadecimal de %d é %08X' % (1500, 1500))
+print('O hexadecimal de %d é %x' % (1500, 1500))
--- utilitarios-python.py
+++ utilitarios-python.py
@@ -12,3 +12,20 @@ print(linha_1)
 # shell interativo em python:
 # digitar no terminal python -i nome_do_arquivo.py
 # quit() ou exit() sai do modo interativo
+
+
+"""
+Interpolação básica de strings (herança de C)
+%s - string
+%d e %i - int
+%f - float
+%x (com letras minusculas no hexadecimal) e %X (com letras maiusculas no hexadecimal) - Hexadecimal (ABCDEF0123456789)
+%06X - seis digitos hexadecimal com minusculas
+"""
+
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
+print('O hexadecimal de %d é %x' % (1500, 1500))
