223ef8eb221bbc8af58fbceee2777cdb73352678
Jorge Harbes <jharbes@hotmail.com>
2023-03-04 12:22:34 -0300

    utilitarios python

--- 03-inicio-python/032-formatandoStringsMetodoFormat.py
+++ 03-inicio-python/032-formatandoStringsMetodoFormat.py
@@ -2,11 +2,23 @@ a = 'AAAAA'
 b = 'BBBBBB'
 c = 1.1
 
-string0 = 'a={} b={} c={}'
-string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
 
-print(string0.format(a,b,c))
+# teste1=''.format(a,b,c)
+# print(teste1)
+
+string0 = 'a={} b={} c={}'
+print(string0.format(a,b,c)) # nao sai nada
+
+print() # pula uma linha por padrao
+
+string1 = 'a={2} b={0} b={1} c={1}'.format(a,b,c) # tambem podemos pegar por indices, fazendo com que fiquem fora da ordem do format
+
+print(string1,'\n')
+
+string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
 print(string)
+
+print()
 
 formato = string.format(
     nome1=a, nome2=b, nome3=c
---
+++ utilitarios-python.py
@@ -0,0 +1,9 @@
+# utilitarios python
+
+
+# casas decimais em string no output
+
+nome='jorge'
+altura=1.82000
+linha_1 = f'{nome} tem {altura:.2f} # output: jorge tem 1.80
+print(linha_1)
