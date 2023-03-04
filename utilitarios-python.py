1153328d98b27a02180a37396a8398bdf9c596c4
Jorge Harbes <jharbes@hotmail.com>
2023-03-04 15:48:03 -0300

    Update utilitarios-python.py

--- utilitarios-python.py
+++ utilitarios-python.py
@@ -7,3 +7,8 @@ nome='jorge'
 altura=1.82000
 linha_1 = f'{nome} tem {altura:.2f} # output: jorge tem 1.80
 print(linha_1)
+
+
+# shell interativo em python:
+# digitar no terminal python -i nome_do_arquivo.py
+# quit() ou exit() sai do modo interativo
