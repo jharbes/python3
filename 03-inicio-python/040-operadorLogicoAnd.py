6c446dceb1cef37c73d8329c69863a63e5543279
Jorge Harbes <jharbes@hotmail.com>
2023-03-04 16:41:17 -0300

    Update 040-operadorLogicoAnd.py

--- 03-inicio-python/040-operadorLogicoAnd.py
+++ 03-inicio-python/040-operadorLogicoAnd.py
@@ -5,7 +5,7 @@
 # Se qualquer valor for considerado falso,
 # a expressão inteira será avaliada naquele valor
 # São considerados falsy (que vc já viu)
-# 0 0.0 '' False
+# 0 (zero int) 0.0 (zero float) '' (string vazia) False
 # Também existe o tipo None que é
 # usado para representar um não valor
 entrada = input('[E]ntrar [S]air: ')
@@ -21,4 +21,7 @@ else:
 # Avaliação de curto circuito
 print(True and False and True)
 print(True and 0 and True) # lembrando que 0 = False
-print(True and 1 and True) # lembrando que 0 = False
+print(True and 1 and True) # lembrando que 1 = True
+
+print(not 1==2)
+print(not not 1!=2)
