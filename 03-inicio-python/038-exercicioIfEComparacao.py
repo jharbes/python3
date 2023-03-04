103082e7e037eaa4ca577ed50cbcfc47ead031f3
Jorge Harbes <jharbes@hotmail.com>
2023-03-04 16:12:58 -0300

    Create 038-exercicioIfEComparacao.py

---
+++ 03-inicio-python/038-exercicioIfEComparacao.py
@@ -0,0 +1,11 @@
+primeiro_valor=input('Digite um valor: ')
+segundo_valor=input('Digite um valor: ')
+
+# observe que a resposta será correta mesmo sem que seja feita a conversao entre string e int/float, pois para essa comparacao o python utilizara a tabela unicode, ou seja, serve também para letras   
+
+if primeiro_valor>segundo_valor:
+    print(f'{primeiro_valor=} é maior do que {segundo_valor=}')
+elif segundo_valor>primeiro_valor:
+    print(f'{segundo_valor=} é maior do que {primeiro_valor=}')
+else:
+    print(f'{primeiro_valor=} é igual a {segundo_valor=}')
