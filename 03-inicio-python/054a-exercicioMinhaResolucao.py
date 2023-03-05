37fc8042a8fcf81aee9ddd0daddcb4014391aacd
Jorge Harbes <jharbes@hotmail.com>
2023-03-05 10:56:17 -0300

    inicio exercicio A

---
+++ 03-inicio-python/054a-exercicioMinhaResolucao.py
@@ -0,0 +1,7 @@
+"""
+Exercicio A:
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
---
+++ vscode/utilitarios-vscode.py
@@ -0,0 +1,7 @@
+"""
+Trocar o cursor entre janelas do editor e terminal no vscode
+
+1 - Ctrl + Shift + ' vai abrir o terminal
+2 - Ctrl + Tab vai voltar o cursor para a área do Editor
+3 - Ctrl + ' vai sai do Editor e voltar pro Terminar
+"""
