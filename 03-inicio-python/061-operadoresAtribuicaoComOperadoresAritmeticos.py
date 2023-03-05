87b99500777710df3b17f8e941de660f4b3283ef
Jorge Harbes <jharbes@hotmail.com>
2023-03-05 17:24:17 -0300

    Update 061-operadoresAtribuicaoComOperadoresAritmeticos.py

--- 03-inicio-python/061-operadoresAtribuicaoComOperadoresAritmeticos.py
+++ 03-inicio-python/061-operadoresAtribuicaoComOperadoresAritmeticos.py
@@ -7,4 +7,14 @@ contador = 10
 ###
 
 contador /= 5
-print(contador)
+print(contador) # 2
+
+numero=10
+numero%=2
+
+print(numero) # 0
+
+nome='jorge'
+nome*=10
+
+print(nome) # jorgejorgejorgejorgejorgejorgejorgejorgejorgejorge
