c11dbece40b5a36b07853d420473e81158a68ae0
Jorge Harbes <jharbes@hotmail.com>
2023-03-05 14:29:46 -0300

    Update 058-tiposBuiltIn-documentacao-tipoImutaveis-metodosString.py

--- 03-inicio-python/058-tiposBuiltIn-documentacao-tipoImutaveis-metodosString.py
+++ 03-inicio-python/058-tiposBuiltIn-documentacao-tipoImutaveis-metodosString.py
@@ -4,7 +4,11 @@ Imutáveis que vimos: str, int, float, bool
 """
 
 string = '1000'
-# outra_variavel = f'{string[:3]}ABC{string[4:]}'
-# print(string)
-# print(outra_variavel)
-print(string.zfill(10))
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel) # 100ABC
+
+print(string.zfill(10)) # 0000001000
+print(type(string.zfill(10))) # string
+
+
