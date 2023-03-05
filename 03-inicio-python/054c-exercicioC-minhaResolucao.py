9285f885fc589035578819cba6436215fe5ec065
Jorge Harbes <jharbes@hotmail.com>
2023-03-05 12:42:43 -0300

    Update 054c-exercicioC-minhaResolucao.py

--- 03-inicio-python/054c-exercicioC-minhaResolucao.py
+++ 03-inicio-python/054c-exercicioC-minhaResolucao.py
@@ -5,3 +5,16 @@ menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
 
+nomeUsuario=input('Digite seu primeiro nome: ')
+
+if nomeUsuario!='':
+    if len(nomeUsuario)<=4:
+        print('Seu nome é curto')
+    elif len(nomeUsuario)==5 or len(nomeUsuario)==6:
+        print('Seu nome é normal')
+    elif len(nomeUsuario)>6:
+        print('Seu nome é muito grande')
+    else:
+        print('Erro inesperado')
+else:
+    print('Você não digitou nada.')
