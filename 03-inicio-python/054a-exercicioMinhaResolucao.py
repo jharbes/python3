82a690fb9bad9cb9b7da6cb164be6916a2fc57ab
Jorge Harbes <jharbes@hotmail.com>
2023-03-05 11:08:44 -0300

    Update 054a-exercicioMinhaResolucao.py

--- 03-inicio-python/054a-exercicioMinhaResolucao.py
+++ 03-inicio-python/054a-exercicioMinhaResolucao.py
@@ -5,3 +5,13 @@ informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
 
+entradaUsuario=input('Digite um numero inteiro: ')
+
+try:
+    numeroInteiro=int(entradaUsuario)
+    if numeroInteiro%2 == 0:
+        print('Número par')
+    else:
+        print('Número ímpar')
+except:
+    print("O número digitado não é um número inteiro.")
