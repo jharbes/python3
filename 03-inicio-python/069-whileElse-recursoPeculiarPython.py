f81c016feb4fd6e4861c8ba5c86b37d097ba4282
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-06 15:58:38 -0300

    Update 069-whileElse-recursoPeculiarPython.py

--- 03-inicio-python/069-whileElse-recursoPeculiarPython.py
+++ 03-inicio-python/069-whileElse-recursoPeculiarPython.py
@@ -1,4 +1,7 @@
 """ while/else """
+
+# o else do while só será executado caso o while complete TODOS OS SEUS LOOPS, no exemplo abaixo como ele SAI DO LAÇO ANTES de completar todos os loops de repeticao ele NAO executará o else 
+
 string = 'Valor qualquer'
 
 i = 0
