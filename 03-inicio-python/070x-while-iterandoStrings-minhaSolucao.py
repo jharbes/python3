4a0faa2e02dd8df12bd517e51d3512b6fc8866ac
Jorge Harbes <jharbes@hotmail.com>
2023-03-07 16:45:29 -0300

    incremento de solucao

--- 03-inicio-python/070x-while-iterandoStrings-minhaSolucao.py
+++ 03-inicio-python/070x-while-iterandoStrings-minhaSolucao.py
@@ -1,21 +1,31 @@
 # Retornar qual letra aparece o maior numero de vezes na string utilizando while na solucao.
 
-frase='O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.'
+frase='O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.a'
 
 fraseLowerSemEspacos=frase.lower().replace(' ','').replace('.','')
 
 contador=0
 numeroMaximoRepeticoes=0
 caractereMaisRepetido=''
+caracateresRepetidos=[]
 
 
 while (len(fraseLowerSemEspacos)>contador):
 
     if fraseLowerSemEspacos.count(fraseLowerSemEspacos[contador])>numeroMaximoRepeticoes:
+
         numeroMaximoRepeticoes=fraseLowerSemEspacos.count(fraseLowerSemEspacos[contador])
         caractereMaisRepetido=fraseLowerSemEspacos[contador]
+        caracateresRepetidos.append(fraseLowerSemEspacos[contador])
+
+    elif fraseLowerSemEspacos.count(fraseLowerSemEspacos[contador])==numeroMaximoRepeticoes and fraseLowerSemEspacos[contador]!=caractereMaisRepetido:
+
+        if fraseLowerSemEspacos[contador] not in caracateresRepetidos:
+            caractereMaisRepetido+=', '+fraseLowerSemEspacos[contador]
+            caracateresRepetidos.append(fraseLowerSemEspacos[contador])
+
     contador+=1
 
 
-print(f'A letra que mais se repetiu foi a "{caractereMaisRepetido}" que foi repetido {numeroMaximoRepeticoes} vezes')
+print(f'A(s) letra(s) que mais se repetiu(ram) foi a "{caractereMaisRepetido}" que foi(ram) repetida(s) {numeroMaximoRepeticoes} vezes')
 
