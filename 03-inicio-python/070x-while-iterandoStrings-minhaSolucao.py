f7116b65924c249a7a4b39a1b0629fa6ed14c005
Jorge Harbes <jharbes@hotmail.com>
2023-03-07 16:58:18 -0300

    Update 070x-while-iterandoStrings-minhaSolucao.py

--- 03-inicio-python/070x-while-iterandoStrings-minhaSolucao.py
+++ 03-inicio-python/070x-while-iterandoStrings-minhaSolucao.py
@@ -1,3 +1,5 @@
+from unidecode import unidecode
+
 # Retornar qual letra aparece o maior numero de vezes na string utilizando while na solucao.
 
 frase='O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.a'
@@ -7,6 +9,9 @@ fraseLowerSemEspacos=frase.lower()
 
 # retirando todos os caracteres alfanumericos da string
 fraseLowerSemEspacos=''.join(ch for ch in fraseLowerSemEspacos if ch.isalnum())
+
+# retirando os acentos das letras (para que letra acentuada posso contar apenas como uma letra normal)
+fraseLowerSemEspacos=unidecode(fraseLowerSemEspacos)
 
 contador=0
 numeroMaximoRepeticoes=0
@@ -22,6 +27,7 @@ while (len(fraseLowerSemEspacos)>contador):
         caractereMaisRepetido=fraseLowerSemEspacos[contador]
         caracateresRepetidos.append(fraseLowerSemEspacos[contador])
 
+    # esse elif serve no caso de termos mais de uma letra repetindo-se com o maior numero de vezes, logo todas ficarao como maiores repeticoes
     elif fraseLowerSemEspacos.count(fraseLowerSemEspacos[contador])==numeroMaximoRepeticoes and fraseLowerSemEspacos[contador]!=caractereMaisRepetido:
 
         if fraseLowerSemEspacos[contador] not in caracateresRepetidos:
