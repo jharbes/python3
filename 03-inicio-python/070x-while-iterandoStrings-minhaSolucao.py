c1a770ac8aff16e8306597be7d953e426bdf2c40
Jorge Harbes <jharbes@hotmail.com>
2023-03-07 17:08:03 -0300

    Update 070x-while-iterandoStrings-minhaSolucao.py

--- 03-inicio-python/070x-while-iterandoStrings-minhaSolucao.py
+++ 03-inicio-python/070x-while-iterandoStrings-minhaSolucao.py
@@ -2,7 +2,7 @@ from unidecode import unidecode
 
 # Retornar qual letra aparece o maior numero de vezes na string utilizando while na solucao.
 
-frase='O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.a'
+frase='O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.'
 
 # colocando em minusculas
 fraseLowerSemEspacos=frase.lower()
@@ -16,7 +16,6 @@ fraseLowerSemEspacos=unidecode(fraseLowerSemEspacos)
 contador=0
 numeroMaximoRepeticoes=0
 caractereMaisRepetido=''
-caracateresRepetidos=[]
 
 
 while (len(fraseLowerSemEspacos)>contador):
@@ -25,6 +24,7 @@ while (len(fraseLowerSemEspacos)>contador):
 
         numeroMaximoRepeticoes=fraseLowerSemEspacos.count(fraseLowerSemEspacos[contador])
         caractereMaisRepetido=fraseLowerSemEspacos[contador]
+        caracateresRepetidos=[]
         caracateresRepetidos.append(fraseLowerSemEspacos[contador])
 
     # esse elif serve no caso de termos mais de uma letra repetindo-se com o maior numero de vezes, logo todas ficarao como maiores repeticoes
@@ -36,6 +36,8 @@ while (len(fraseLowerSemEspacos)>contador):
 
     contador+=1
 
-
-print(f'A(s) letra(s) que mais se repetiu(ram) foi a "{caractereMaisRepetido}" que foi(ram) repetida(s) {numeroMaximoRepeticoes} vezes')
+if (len(caracateresRepetidos)>1):
+    print(f'As letras que mais se repetiram foram as "{caractereMaisRepetido}" em um total de {numeroMaximoRepeticoes} vezes')
+else:
+    print(f'A letras que mais se repetiu foi a "{caractereMaisRepetido}" em um total de {numeroMaximoRepeticoes} vezes')
 
