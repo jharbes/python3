c25b7f4117828ac261d06af735c70734787e4802
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-08 17:04:12 -0300

    Update 086-exercicio-exibaOsIndicesDaLista.py

--- 03-inicio-python/086-exercicio-exibaOsIndicesDaLista.py
+++ 03-inicio-python/086-exercicio-exibaOsIndicesDaLista.py
@@ -12,4 +12,14 @@ lista.append('João')
 indices = range(len(lista))
 
 for indice in indices:
+    print(indice, lista[indice])
+
+print('---------------------------')
+
+for indice in indices:
     print(indice, lista[indice], type(lista[indice]))
+
+print('---------------------------')
+
+for indice,nome in enumerate(lista):
+    print(indice, nome)
