202d0a62f92a5c1d350797167ed801710d68fceb
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-08 16:42:19 -0300

    Update 084-list-copy-cuidadosComDadosMutaveis.py

--- 03-inicio-python/084-list-copy-cuidadosComDadosMutaveis.py
+++ 03-inicio-python/084-list-copy-cuidadosComDadosMutaveis.py
@@ -3,6 +3,20 @@ Cuidados com dados mutáveis
 = - copiado o valor (imutáveis)
 = - aponta para o mesmo valor na memória (mutável)
 """
+
+listaC=[1,2,3,4]
+listaD=listaC # observe que ao igualar a variavel lista D apenas aponta para o endereco de memoria de listaC, ela nao COPIA os items
+
+print(listaD)
+
+listaD[0]=10 # sendo assim ao alterar o indice 0 estamos alterando o endereco de memoria do indice 0, consequentemente alterando em ambas as listas, listaD e listaC
+print(listaC)
+print(listaD)
+
+print('--------------------------')
+
+
+# no caso do copy será diferente, a lista é COPIADA e o endereco de cada uma na memoria será independente, ou seja, ao alterar uma a outra nao será alterada
 lista_a = ['Luiz', 'Maria', 1, True, 1.2]
 lista_b = lista_a.copy()
 
