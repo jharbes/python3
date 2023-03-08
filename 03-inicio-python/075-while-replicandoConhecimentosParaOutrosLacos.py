d1f15e12d43ac7eb18cfa6518cf75bbf4dcaf7a4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-08 10:22:17 -0300

    Update 075-while-replicandoConhecimentosParaOutrosLacos.py

--- 03-inicio-python/075-while-replicandoConhecimentosParaOutrosLacos.py
+++ 03-inicio-python/075-while-replicandoConhecimentosParaOutrosLacos.py
@@ -1,3 +1,12 @@
+# Aqui deixamos claro que os comandos vistos anteriormente como continue, break e o else no fim também funcionam no for como funcionavam no while
+
+# continue - pula pra proxima repeticao
+
+# break - cai pra fora do laco
+
+# else -  executa o que tem no else apenas se o for chegar ao seu fim com todas as execucoes completas
+
+
 for i in range(10):
     if i == 2:
         print('i é 2, pulando...')
