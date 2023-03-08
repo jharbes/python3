e6f8b291f99ba3f6c9f9c2d5efbfcab6a0e00434
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-08 16:55:14 -0300

    Update 085-forInComTipoList.py

--- 03-inicio-python/085-forInComTipoList.py
+++ 03-inicio-python/085-forInComTipoList.py
@@ -5,3 +5,8 @@ lista = ['Maria', 'Helena', 'Luiz']
 
 for nome in lista:
     print(nome, type(nome))
+
+
+# *** IMPORTANTE: for in com acesso ao indice
+for indice,nome in enumerate(lista):
+    print(indice, nome)
