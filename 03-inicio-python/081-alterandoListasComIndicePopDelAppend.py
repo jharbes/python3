d6acc7936e33ea2f544e2c81fee78f48112f126e
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-08 15:36:29 -0300

    Update 081-alterandoListasComIndicePopDelAppend.py

--- 03-inicio-python/081-alterandoListasComIndicePopDelAppend.py
+++ 03-inicio-python/081-alterandoListasComIndicePopDelAppend.py
@@ -3,6 +3,7 @@ Listas em Python
 Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
+
 Métodos úteis:
     append, insert, pop, del, clear, extend, +
 Create Read Update   Delete
@@ -10,13 +11,24 @@ Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
 #        0   1   2   3   4   5
 lista = [10, 20, 30, 40]
-# lista[2] = 300
-# del lista[2]
-# print(lista)
-# print(lista[2])
-lista.append(50)
-lista.pop()
+lista[2] = 300
+print(lista) # [10, 20, 300, 40]
+
+del lista[2] # apaga o elemento da lista
+print(lista) # [10, 20, 40]
+
+print(lista[2]) # 40
+
+lista.append(50) # adiciona o elemento no fim da lista
+print(lista) # [10, 20, 40, 50]
+
+lista.pop() # apaga o ultimo elemento da lista
+print(lista) # [10, 20, 40]
+
 lista.append(60)
 lista.append(70)
-ultimo_valor = lista.pop(3)
+print(lista)
+
+ultimo_valor = lista.pop(3) # apaga o elemento de indice 3 E TAMBEM retorna o valor apagado
+
 print(lista, 'Removido,', ultimo_valor)
