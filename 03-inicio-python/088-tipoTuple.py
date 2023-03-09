a18c91abbf2998ef4264c56f40f70bd67d5cf3d0
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-09 10:37:39 -0300

    Update 088-tipoTuple.py

--- 03-inicio-python/088-tipoTuple.py
+++ 03-inicio-python/088-tipoTuple.py
@@ -2,7 +2,13 @@
 Tipo tupla - Uma lista imutável
 """
 nomes = ('Maria', 'Helena', 'Luiz')
-# nomes = tuple(nomes)
-# nomes = list(nomes)
-print(nomes[-1])
-print(nomes)
+nomes2 = 'Maria', 'Helena', 'Luiz' # a ausencia dos colchetes tambem cria uma tupla
+nomes3 = ['Maria', 'Helena', 'Luiz']
+
+nomes3 = tuple(nomes) # converte de lista para tupla
+nomes2 = list(nomes) # converte de tupla para lista
+
+print(nomes[-1]) # Luiz
+print(nomes) # ('Maria', 'Helena', 'Luiz')
+print(nomes2) # ['Maria', 'Helena', 'Luiz']  -- ja convertido para lista
+print(nomes3)
