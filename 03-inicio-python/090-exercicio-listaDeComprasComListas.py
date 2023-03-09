7a9ca6a716fd8c6e3501891a8de4431c390924ae
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-09 11:25:42 -0300

    Create 090-exercicio-listaDeComprasComListas.py

---
+++ 03-inicio-python/090-exercicio-listaDeComprasComListas.py
@@ -0,0 +1,4 @@
+"""
+Faça uma lista de compras com listas, o usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista.
+Não permita que o programa quebre com erros de indices inexistentes na lista.
+"""
