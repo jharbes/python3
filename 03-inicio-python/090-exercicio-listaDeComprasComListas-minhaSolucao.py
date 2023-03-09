4cd95dfab475217397d36762ef67e179be0b4bb2
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-09 11:44:39 -0300

    acerto de nome, final da minha solucao

---
+++ 03-inicio-python/090-exercicio-listaDeComprasComListas-minhaSolucao.py
@@ -0,0 +1,44 @@
+"""
+Faça uma lista de compras com listas, o usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista.
+Não permita que o programa quebre com erros de indices inexistentes na lista.
+"""
+
+listaCompras=[]
+
+while True:
+    print('LISTA DE COMPRAS\n')
+    print('Opções disponíveis:')
+    print('1- Inserir item na lista.')
+    print('2- Excluir item da lista.')
+    print('3- Listar itens da lista')
+    print('0- Sair do programa\n')
+
+    opcao=input('Digite a opção desejada: ')
+    
+    if opcao=='1':
+        item=input('\nDigite o nome do item a ser adicionado na lista: ')
+        listaCompras.append(item)
+        continue
+    elif opcao=='2':
+        try:
+            opcaoExclusao=input('\nSelecione o numero do item a ser excluido: ')
+            opcaoExclusao=int(opcaoExclusao)
+            print(f'\nItem {listaCompras.pop(opcaoExclusao-1)} excluído com sucesso!\n')
+            print()
+        except:
+            print('\nNumero de item invalido!\n')
+    elif opcao=='3':
+        print()
+        if len(listaCompras)>=1:
+            for indice,item in enumerate(listaCompras):
+                print(f'Item nº{indice+1}: {item}')
+            print()
+        else:
+            print('A lista está vazia.\n')
+        continue
+    elif opcao=='0':
+        print('\nSaindo do programa...\n')
+        break
+    else:
+        print('\nOpção inválida.\n')
+        continue
--- 03-inicio-python/090-exercicio-listaDeComprasComListas.py
+++
@@ -1,4 +0,0 @@
-"""
-Faça uma lista de compras com listas, o usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista.
-Não permita que o programa quebre com erros de indices inexistentes na lista.
-"""
