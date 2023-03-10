1090009d36712644d4e5bf1002f21ddd49b9a9c7
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-10 15:26:21 -0300

    Update 097-operacaoTernariaEmPython.py

--- 03-inicio-python/097-operacaoTernariaEmPython.py
+++ 03-inicio-python/097-operacaoTernariaEmPython.py
@@ -1,7 +1,22 @@
 """
+
 Operação ternária (condicional de uma linha)
 <valor> if <condicao> else <outro valor>
+
+outras linguagens:
+
+condicao ? valor se true : valor se false
+
+python:
+
+(valor se true) if (condicao) else (valor se false)
+
 """
+
+print('joao\n' if 2>0 else 'carlos\n')
+
+print('joao\n' if -2>0 else 'carlos\n')
+
 condicao = 10 == 11 # false
 variavel = 'Valor' if condicao else 'Outro valor' # Outro valor
 print(variavel) # Outro valor
@@ -13,12 +28,12 @@ novo_digito = digito if digito <= 9 else 0 # 9
 novo_digito = 0 if digito > 9 else digito
 print(novo_digito) # 9
 
-print('----------------------------------')
+print('-------------------------------------')
 
+# operacao ternaria composta
+print('Valor' if False else 'Outro valor' if False else 'Fim') # Fim
 
-print('Valor' if False else 'Outro valor' if False else 'Fim')
-
-print('----------------------------------')
+print('-------------------------------------')
 
 x=4
 print('deu certo') if x == 2 else print('deu errado')
