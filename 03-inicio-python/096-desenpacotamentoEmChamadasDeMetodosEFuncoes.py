eba439886a56f6961ee953ec79fcccadd1ef9c90
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-10 15:04:47 -0300

    Update 096-desenpacotamentoEmChamadasDeMetodosEFuncoes.py

--- 03-inicio-python/096-desenpacotamentoEmChamadasDeMetodosEFuncoes.py
+++ 03-inicio-python/096-desenpacotamentoEmChamadasDeMetodosEFuncoes.py
@@ -12,12 +12,23 @@ salas = [
     ['Luiz', 'João', 'Eduarda', ],  # 2
 ]
 
-# p, b, *_, ap, u = lista
-# print(p, u, ap)
+# nesse caso p=lista[0], b=lista[1], *_=lista[2] e lista[3], ap=lista[4] e u=lista[5]
+p, b, *_, ap, u = lista
+print(*_) # 1 2
+print(p, u, ap) # Maria Eduarda 3
 
-# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
-# print(*lista)
-# print(*string)
-# print(*tupla)
+print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+print(*lista) # Maria Helena 1 2 3 Eduarda
+print(*string) # A B C D
+print(*tupla) # Python é legal
 
+# sep => separador (vai alterar o separador de espaco para pular linha)
 print(*salas, sep='\n')
+"""
+output:
+
+['Maria', 'Helena']
+['Elaine']
+['Luiz', 'João', 'Eduarda']
+
+"""
