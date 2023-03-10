5e6087aeefbab1d177fd5cf75b8af4df866b041d
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-10 10:32:45 -0300

    Update 093-split-join-strip-metodosUteisParaStringEListas.py

--- 03-inicio-python/093-split-join-strip-metodosUteisParaStringEListas.py
+++ 03-inicio-python/093-split-join-strip-metodosUteisParaStringEListas.py
@@ -3,14 +3,32 @@ split e join com list e str
 split - divide uma string (list)
 join - une uma string
 """
+
+frase1='Testando conversores em python'
+frase1Separada=frase1.split()
+
+# A ausencia do argumento no split faz com que ele divida a string em um array com base nos espacos existentes entre ela ' ', isso inclui \n \t \r \f
+print(frase1Separada) # ['Testando', 'conversores', 'em', 'python']
+
+print('--------------------------------------')
+
 frase = '   Olha só que   , coisa interessante          '
 lista_frases_cruas = frase.split(',')
+print(lista_frases_cruas) # ['   Olha só que   ', ' coisa interessante          ']
 
+# como vemos abaixo split nao altera a frase original
+print(frase) #    Olha só que   , coisa interessante
+
+print('--------------------------------------')
+
+# o metodo strip corta os espacos do comeco e do fim da string (inclusive os caracteres especiais \n \t \v \f), tambem existem os metodos lstrip e rstrip que funcionam para os espacos e caracteres especiais à esquerda da string e direita da string respectivamente
 lista_frases = []
 for i, frase in enumerate(lista_frases_cruas):
     lista_frases.append(lista_frases_cruas[i].strip())
 
-# print(lista_frases_cruas)
-# print(lista_frases)
+print(lista_frases) # ['Olha só que', 'coisa interessante'] 
+
+print('--------------------------------------')
+
 frases_unidas = ', '.join(lista_frases)
-print(frases_unidas)
+print(frases_unidas) # Olha só que, coisa interessante
