dc57daf5a307b3f4c4c444ef9af441cdbd780056
Jorge Harbes <jharbes@hotmail.com>
2023-03-12 16:44:04 -0300

    Update 114-highOrderFunctions-FuncoesDePrimeiraClasse.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/114-highOrderFunctions-FuncoesDePrimeiraClasse.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/114-highOrderFunctions-FuncoesDePrimeiraClasse.py
@@ -1,6 +1,12 @@
 """
+
 Higher Order Functions
 Funções de primeira classe
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+
 """
 
 
