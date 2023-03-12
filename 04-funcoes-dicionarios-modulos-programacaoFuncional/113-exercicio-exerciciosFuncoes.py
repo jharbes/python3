4412e88ddd62f60d58879942118733e4123f1d1f
Jorge Harbes <jharbes@hotmail.com>
2023-03-12 15:18:31 -0300

    Update 113-exercicio-exerciciosFuncoes.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/113-exercicio-exerciciosFuncoes.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/113-exercicio-exerciciosFuncoes.py
@@ -5,10 +5,19 @@
 # Retorne o total para uma variável e mostre o valor
 # da variável.
 
+def multiplica(*args):
+    totalMultiplicacao=1
+    for numero in args:
+        totalMultiplicacao*=numero
+    return totalMultiplicacao
 
+variavel1=multiplica(2,3,4,5,6)
+print(variavel1)
 
 
 
 
 # Crie uma função fala se um número é par ou ímpar.
 # Retorne se o número é par ou ímpar.
+
+
