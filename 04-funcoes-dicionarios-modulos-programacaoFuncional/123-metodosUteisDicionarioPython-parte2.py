d063789ed45baa7f76f6741daad4b0aab2fdc2f2
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-13 16:01:06 -0300

    Update 123-metodosUteisDicionarioPython-parte2.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/123-metodosUteisDicionarioPython-parte2.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/123-metodosUteisDicionarioPython-parte2.py
@@ -9,35 +9,25 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
-pessoa = {
-    'nome': 'Luiz Otávio',
+p1 = {
+    'nome': 'Luiz',
     'sobrenome': 'Miranda',
-    'idade': 900,
 }
+# print(p1['nome'])
+# print(p1.get('nome', 'Não existe'))
 
-pessoa.setdefault('idade', 0)
-print(pessoa['idade']) # 900
-print(len(pessoa)) # 3
-
-# observe que os metodos abaixos estao sendo convertidos em lista para facilitar sua utilizacao, pois o retorno original eh o dictkeys que nao facilita muito o seu uso
-
-print(pessoa.keys()) # dict_keys(['nome', 'sobrenome', 'idade'])
-print(list(pessoa.keys())) # ['nome', 'sobrenome', 'idade']
-
-print(pessoa.values()) # dict_values(['Luiz Otávio', 'Miranda', 900])
-print(list(pessoa.values())) # ['Luiz Otávio', 'Miranda', 900]
-
-print(pessoa.items()) # dict_items([('nome', 'Luiz Otávio'), ('sobrenome', 'Miranda'), ('idade', 900)])
-print(list(pessoa.items())) # [('nome', 'Luiz Otávio'), ('sobrenome', 'Miranda'), ('idade', 900)]
-
-print('---------------------------')
-
-# for direto, sem coercao(convesao em lista ou tupla)
-for valor in pessoa.values(): 
-    print(valor)
-
-print('---------------------------')
-
-# for direto, sem coercao(convesao em lista ou tupla)
-for chave, valor in pessoa.items():
-    print(chave, valor)
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
+# p1.update({
+#     'nome': 'novo valor',
+#     'idade': 30,
+# })
+# p1.update(nome='novo valor', idade=30)
+# tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
