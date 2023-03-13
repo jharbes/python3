487f6951f43ee877bba8bbf51fd412fbf30318dd
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-13 15:58:26 -0300

    Update 122-shallowCopy-deepCopy-dadosMutaveis.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/122-shallowCopy-deepCopy-dadosMutaveis.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/122-shallowCopy-deepCopy-dadosMutaveis.py
@@ -9,22 +9,32 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
-import copy
+
+import copy # nesse modulo temos como fazer uma deep copy (copia total)
 
 d1 = {
     'c1': 1,
     'c2': 2,
     'l1': [0, 1, 2],
 }
-d2 = d1.copy()
 
-print(d1)
-print(d2)
+d3 = d1 # nao é copia, apenas aponta para o mesmo local na memoria (valores mutaveis)
+
+d2 = d1.copy() # copia rasa - shallow copy
+d4 = copy.copy(d1) # comando igual ao anterior fazendo copia rasa
+
+d5 = copy.deepcopy(d1) # nesse caso será uma deep copy (copia profunda) onde nao havera mais direcionamento ao mesmo local da memoria
+
+print(d1) # {'c1': 1, 'c2': 2, 'l1': [0, 1, 2]}
+print(d2) # {'c1': 1, 'c2': 2, 'l1': [0, 1, 2]}
+print(d5) # {'c1': 1, 'c2': 2, 'l1': [0, 1, 2]}
 
 print('---------------------------------')
 
 d2['c1'] = 1000
 d2['l1'][1] = 999999
-
-print(d1)
-print(d2)
+d5['l1'][0]=55
+# observe que o valor no meio da lista foi alterado em ambos os dicionarios pois nesse caso sao dados mutaveis, configurando a copia feita como uma copia rasa
+print(d1) # {'c1': 1, 'c2': 2, 'l1': [0, 999999, 2]}
+print(d2) # {'c1': 1000, 'c2': 2, 'l1': [0, 999999, 2]}
+print(d5) # {'c1': 1, 'c2': 2, 'l1': [55, 1, 2]}
