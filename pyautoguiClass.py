2d1053188ca42fa5f18478d6681ba5424a28a574
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-14 11:41:09 -0300

    Create pyautoguiClass.py

---
+++ pyautoguiClass.py
@@ -0,0 +1,9 @@
+# Utilizando biblioteca de automacao pyautogui
+
+import pyautogui
+
+pyautogui.click # clique com o mouse
+pyautogui.write # escrever um texto
+pyautogui.press # apertar uma tecla
+pyautogui.hotkey # apertar uma combinacao de teclas, ex: CTRL+D
+
