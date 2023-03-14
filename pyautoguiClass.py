ccd0bb529dea51647524723805c7672cc29debb5
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-14 13:18:33 -0300

    Update pyautoguiClass.py

--- pyautoguiClass.py
+++ pyautoguiClass.py
@@ -1,9 +1,26 @@
 # Utilizando biblioteca de automacao pyautogui
 
 import pyautogui
+import time # para usar o time.sleep()
 
 pyautogui.click # clique com o mouse
 pyautogui.write # escrever um texto
 pyautogui.press # apertar uma tecla
 pyautogui.hotkey # apertar uma combinacao de teclas, ex: CTRL+D
 
+# Passo a passo
+
+# Passo 1: Entrar no sistema da empresa (no link)
+
+## Com o navegador fechado:
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(3) # faz esperar dois segundos antes do proximo comando
+pyautogui.write('https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema')
+pyautogui.press('enter')
+
+## Com o navegador aberto:
+# pyautogui.hotkey('ctrl', 't')
+# pyautogui.write('https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema')
+# pyautogui.press('enter')
