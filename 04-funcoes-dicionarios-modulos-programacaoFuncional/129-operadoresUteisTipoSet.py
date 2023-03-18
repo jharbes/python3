9be45707c641a923b2cf54fc1a3419a37864c93a
Jorge Harbes <jharbes@hotmail.com>
2023-03-18 16:25:58 -0300

    Update 129-operadoresUteisTipoSet.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/129-operadoresUteisTipoSet.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/129-operadoresUteisTipoSet.py
@@ -5,8 +5,17 @@
 # diferença simétrica ^ - Itens que não estão em ambos
 s1 = {1, 2, 3}
 s2 = {2, 3, 4}
-s3 = s1 | s2
+
+s3 = s1 | s2 
+print(s3) # {1, 2, 3, 4}
+
 s3 = s1 & s2
+print(s3) # {2, 3}
+
+# na diferenca a ordem importa
 s3 = s2 - s1
+print(s3) # {4}
+
+# itens que nao estao nos dois (ao mesmo tempo), ou seja s1 + s2 - (s1 & s2)
 s3 = s1 ^ s2
-print(s3)
+print(s3) # {1, 4}
