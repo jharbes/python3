f50b43231e8ab267a3a96a588a919bb768888d7e
Jorge Harbes <jharbes@hotmail.com>
2023-03-18 15:51:55 -0300

    Update 128-metodosUteisTipoSet.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/128-metodosUteisTipoSet.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/128-metodosUteisTipoSet.py
@@ -3,11 +3,16 @@
 s1 = set()
 s1.add('Luiz')
 s1.add(1)
-s1.update(('Olá mundo', 1, 2, 3, 4))
-# s1.clear()
+s1.update(('Olá mundo', 1, 2, 3, 4)) # dessa maneira, adicionando essa tupla, colocaremos os elemntos um a um pois os iteraveis sao adicionados elemento a elemento, se adicionarmos uma palavra pura string serao adicionados as letras separadamente (no caso de mais de um elemento)
+print(s1) # {1, 2, 3, 4, 'Luiz', 'Olá mundo'}
+
+s2=set()
+s2.add('Luiz')
+print(s2) # {'Luiz'}    
+# s1.clear() # Limpa o set
 s1.discard('Olá mundo')
 s1.discard('Luiz')
-print(s1)
+print(s1) # {1, 2, 3, 4}
 
 # Operadores úteis:
 # união | união (union) - Une
