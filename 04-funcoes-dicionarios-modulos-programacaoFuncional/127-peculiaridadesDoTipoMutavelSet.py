900eac220197c5e7af28377bb213d4af1d683a73
Jorge Harbes <jharbes@hotmail.com>
2023-03-18 06:09:54 -0300

    Update 127-peculiaridadesDoTipoMutavelSet.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/127-peculiaridadesDoTipoMutavelSet.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/127-peculiaridadesDoTipoMutavelSet.py
@@ -18,13 +18,29 @@
 # - não tem índexes;
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
-# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
-# s1 = set(l1)
-# l2 = list(s1)
-# s1 = {1, 2, 3}
-# print(3 not in s1)
-# for numero in s1:
-#     print(numero)
+
+s1={1,2,3,3,3,3,3,1}
+print(s1) # {1 ,2 , 3} - observe que o set naturalmente ja exclui os elementos repetidos
+
+# ELIMINANDO ELEMENTOS REPETIDOS COM SET *** (observe que a ordem pode nao ser garantida com set)
+l1=[1,2,3,3,3,3,3,1]
+s2=set(l1)
+l2=list(s2)
+print(l2,type(l2)) # [1, 2, 3] <class 'list'>
+
+# s4={1,2,3,{123}} ERRO -> sets nao aceitam tipos mutaveis como seus elementos
+
+# print(s1[0]) ERRO -> sets nao tem indice
+
+
+s3 = {1, 2, 3}
+print(3 not in s1) # False
+print(2 in s1) # True
+print(0 in s1) # False
+
+
+for numero in s1:
+    print(numero)
 
 # Métodos úteis:
 # add, update, clear, discard
