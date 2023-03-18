52445cf3f0217a739d3907fc6f53dd1bc587cba6
Jorge Harbes <jharbes@hotmail.com>
2023-03-18 05:45:35 -0300

    Update 126-set-introducaoAoTipoSet.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/126-set-introducaoAoTipoSet.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/126-set-introducaoAoTipoSet.py
@@ -9,7 +9,23 @@
 # set(iterável) ou {1, 2, 3}
 # s1 = set('Luiz')
 s1 = set()  # vazio
+print(s1,type(s1)) # set() <class 'set'>
+
+s2={} # CUIDADO! nesse caso nao cria um set e sim um dicionario
+print(s2,type(s2)) # {} <class 'dict'>  
+
 s1 = {'Luiz', 1, 2, 3}  # com dados
+print(s1) # {1, 2, 3, 'Luiz'}  
+
+s3={1,2,3}
+print(s3,type(s3)) # {1, 2, 3} <class 'set'>
+
+# nessa maneira de criacao de set ele vai iterar sobre a palavra luiz, e nao vai garantir a ordem dos elementos
+s4=set('Luiz')
+print(s4,type(s4)) # {'u', 'L', 'z', 'i'}  
+
+s5={'Luiz'}
+print(s5,type(s5)) # {'Luiz'} <class 'set'>
 
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
