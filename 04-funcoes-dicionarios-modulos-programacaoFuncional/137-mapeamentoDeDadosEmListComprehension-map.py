7221eb270c8dacc039c35744177e2eea0576e9ab
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-23 11:14:51 -0300

    Update 137-mapeamentoDeDadosEmListComprehension-map.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/137-mapeamentoDeDadosEmListComprehension-map.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/137-mapeamentoDeDadosEmListComprehension-map.py
@@ -1,6 +1,7 @@
 # Introdução à List comprehension em Python
 # List comprehension é uma forma rápida para criar listas
 # a partir de iteráveis.
+
 # print(list(range(10)))
 lista = []
 for numero in range(10):
@@ -20,11 +21,28 @@ produtos = [
     {'nome': 'p2', 'preco': 10, },
     {'nome': 'p3', 'preco': 30, },
 ]
+print(produtos)
+
+print('----------------------------------')
+
+produtos2=[
+    {**produto} for produto in produtos
+]
+print(produtos2)
+
+print('----------------------------------')
+
+
+# desempacotando produto dentro das chaves onde o preco do produto tera acrescimo de 5% quando o valor do preco for maior que 20
+# a lista nova deve ter o mesmo numero de elementos da lista antiga, isso caracteriza o mapeamento
 novos_produtos = [
     {**produto, 'preco': produto['preco'] * 1.05}
     if produto['preco'] > 20 else {**produto}
     for produto in produtos
 ]
 
-# print(novos_produtos)
+print(novos_produtos)
+
+print('----------------------------------')
+
 print(*novos_produtos, sep='\n')
