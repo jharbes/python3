ad0d6f6ed95a13b81781db1f6c3781f3fe5e7d76
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-23 13:24:16 -0300

    Update 139-listComprehensionComMaisDeUmFor.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/139-listComprehensionComMaisDeUmFor.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/139-listComprehensionComMaisDeUmFor.py
@@ -1,15 +1,27 @@
+# LIST COMPREHENSION COM MAIS DE UM FOR
+
 lista = []
 for x in range(3):
     for y in range(3):
         lista.append((x, y))
-lista = [
+
+print(lista)
+
+print('------------------------------------')
+
+lista2 = [
     (x, y)
     for x in range(3)
     for y in range(3)
 ]
-lista = [
+
+print(lista2)
+
+print('------------------------------------')
+
+lista3 = [
     [(x, letra) for letra in 'Luiz']
     for x in range(3)
 ]
 
-print(lista)
+print(lista3)
