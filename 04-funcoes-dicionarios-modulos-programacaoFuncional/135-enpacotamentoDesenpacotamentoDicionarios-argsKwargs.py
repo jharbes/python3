79e6a9ba53f282c008b5556c26edbf513b204a9d
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-23 10:45:05 -0300

    Update 135-enpacotamentoDesenpacotamentoDicionarios-argsKwargs.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/135-enpacotamentoDesenpacotamentoDicionarios-argsKwargs.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/135-enpacotamentoDesenpacotamentoDicionarios-argsKwargs.py
@@ -5,9 +5,6 @@ print(a, b) # 2 1
 
 print('----------------------------')
 
-# for chave, valor in pessoa.items():
-#     print(chave, valor)
-
 pessoa = {
     'nome': 'Aline',
     'sobrenome': 'Souza',
@@ -17,17 +14,30 @@ pessoa = {
 a,b=pessoa
 print(a,b) # nome sobrenome
 
+print('----------------------------')
+
 # desenpacotamento dos values
 a,b=pessoa.values()
 print(a,b) # Aline Souza
+
+print('----------------------------')
 
 # ambos os valores, keys e values
 a,b=pessoa.items()
 print(a,b) # ('nome', 'Aline') ('sobrenome', 'Souza')
 
+print('----------------------------')
+
 (a1, a2), (b1, b2) = pessoa.items()
 print(a1, a2) # nome Aline
 print(b1, b2) # sobrenome Souza
+
+print('----------------------------')
+
+for chave, valor in pessoa.items():
+    print(chave, valor)
+# nome Aline
+# sobrenome Souza
 
 print('----------------------------')
 
@@ -36,14 +46,24 @@ dados_pessoa = {
     'altura': 1.6,
 }
 
-pessoas_completa = {**pessoa, **dados_pessoa}
-# print(pessoas_completa)
+# nesse caso os dois ** extrai os dados dos dicionarios de origem (pessoa, dados_pessoa) dentro do novo dicionario (pessoas_completa) gerando um novo dicionario com os dados dos dois primeiros
+pessoa_completa = {**pessoa, **dados_pessoa}
+print(pessoa_completa) # {'nome': 'Aline', 'sobrenome': 'Souza', 'idade': 16, 'altura': 1.6}
+
+
+# nesse caso adicionamos mais um key, caso o key ja exista o value referente a ele sera sobrescrito
+pessoa_completa={**pessoa_completa, 'departamento': 'logistica'}
+print(pessoa_completa)
+# {'nome': 'Aline', 'sobrenome': 'Souza', 'idade': 16, 'altura': 1.6, 'departamento': 'logistica'}
+
+
+print('----------------------------')
 
 # args e kwargs
 # args (já vimos)
 # kwargs - keyword arguments (argumentos nomeados)
 
-
+# kwargs sempre sera com dois asteristicos
 def mostro_argumentos_nomeados(*args, **kwargs):
     print('NÃO NOMEADOS:', args)
 
@@ -51,8 +71,12 @@ def mostro_argumentos_nomeados(*args, **kwargs):
         print(chave, valor)
 
 
-# mostro_argumentos_nomeados(nome='Joana', qlq=123)
-# mostro_argumentos_nomeados(**pessoas_completa)
+mostro_argumentos_nomeados(nome='Joana', qlq=123)
+
+
+mostro_argumentos_nomeados(**pessoa_completa)
+
+print('----------------------------')
 
 configuracoes = {
     'arg1': 1,
