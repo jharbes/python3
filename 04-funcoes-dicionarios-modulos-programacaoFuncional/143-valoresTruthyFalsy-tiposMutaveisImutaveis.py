0b09da44b7f3cb27af56d4f63e9407e6de79dba1
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-24 12:02:42 -0300

    Update 143-valoresTruthyFalsy-tiposMutaveisImutaveis.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/143-valoresTruthyFalsy-tiposMutaveisImutaveis.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/143-valoresTruthyFalsy-tiposMutaveisImutaveis.py
@@ -6,7 +6,7 @@ dicionario = {}
 conjunto = set()
 tupla = ()
 string = ''
-inteito = 0
+inteiro = 0
 flutuante = 0.0
 nada = None
 falso = False
@@ -14,7 +14,7 @@ intervalo = range(0)
 
 
 def falsy(valor):
-    return 'falsy'if not valor else 'truthy'
+    return 'falsy' if not valor else 'truthy'
 
 
 print(f'TESTE', falsy('TESTE'))
@@ -23,8 +23,24 @@ print(f'{dicionario=}', falsy(dicionario))
 print(f'{conjunto=}', falsy(conjunto))
 print(f'{tupla=}', falsy(tupla))
 print(f'{string=}', falsy(string))
-print(f'{inteito=}', falsy(inteito))
+print(f'{inteiro=}', falsy(inteiro))
 print(f'{flutuante=}', falsy(flutuante))
 print(f'{nada=}', falsy(nada))
 print(f'{falso=}', falsy(falso))
 print(f'{intervalo=}', falsy(intervalo))
+
+print('--------------------------------')
+
+print(not not 0)
+print(not not 1)
+print(not not "teste")
+print(not not [])
+print(not not [1,2,3])
+print(not not '')
+print(not not 0.0)
+print(not not None)
+print(not not range(0,5))
+print(not not True)
+
+teste=range(10)
+print(teste, type(teste))
