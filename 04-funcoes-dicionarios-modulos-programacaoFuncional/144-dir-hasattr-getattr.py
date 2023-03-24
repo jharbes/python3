7dee8be7c2807903260c6f30f7c5a997b988f85f
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-24 13:37:11 -0300

    Update 144-dir-hasattr-getattr.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/144-dir-hasattr-getattr.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/144-dir-hasattr-getattr.py
@@ -1,9 +1,17 @@
-# dir, hasattr e getattr em Python
+# dir, hasattr, setattr e getattr em Python
+
+# dir(objeto) - lista todos os atributos definidos para o objeto
+
+# hasattr(objeto,metodo) - confere se o objeto possui aquele metodo/atributo
+
+# getattr(objeto,metodo) - busca aquele metodo/atributo
+
 string = 'Luiz'
-metodo = 'strip'
+metodo = 'upper'
 
 if hasattr(string, metodo):
     print('Existe upper')
-    print(getattr(string, metodo)())
+    upperX=print(getattr(string, metodo)) # buscando o metodo referente às strings salvas, podemos jogar em uma variavel 
+    print(getattr(string, metodo)()) # maneira de chamar um metodo cujo nome esteja numa variavel
 else:
     print('Não existe o método', metodo)
