6a3906753d1211f0c5cccf9272222a51dd1e90fa
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-24 16:31:20 -0300

    Update 147-introducaoAsGeneratorFunctions.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/147-introducaoAsGeneratorFunctions.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/147-introducaoAsGeneratorFunctions.py
@@ -3,13 +3,33 @@
 
 def generator(n=0, maximum=10):
     while True:
-        yield n
+        yield n # yield pausa a execucao da funcao, se fizermos next nesse ponto do generator ele retornara n e depois somara 1 a n conforme o codigo ate que n seja menor que o maximum
         n += 1
 
         if n >= maximum:
-            return
+            return # levanta uma excecao de StopIteration com o valor do return que houver (nao obrigatorio)
 
 
 gen = generator(maximum=1000000)
-for n in gen:
+
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
+print('-------------------------------')
+
+gen2=generator(maximum=5)
+
+print(next(gen2))
+print(next(gen2))
+print(next(gen2))
+print(next(gen2))
+print(next(gen2))
+# print(next(gen2)) # erro, StopIteration
+
+print('-------------------------------')
+
+gen3=generator()
+
+for n in gen3: # retorna/finaliza quando n=10 (nao imprime o 10)
     print(n)
