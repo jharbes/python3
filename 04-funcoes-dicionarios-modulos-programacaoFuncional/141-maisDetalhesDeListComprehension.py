92fc0776db4c0cf5b5f5cafec2e9159c592ff1c8
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-24 08:58:44 -0300

    Update 141-maisDetalhesDeListComprehension.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/141-maisDetalhesDeListComprehension.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/141-maisDetalhesDeListComprehension.py
@@ -10,3 +10,28 @@ novosNumeros=numeros # nao gera copia! apenas aponta para o mesmo valor na memor
 numeros[0]=20
 print(novosNumeros) # [20, 2, 3, 4, 5]
 
+print('-------------------------------')
+
+# no caso de dados imutaveis internos (como uma lista de numeros) a copia rasa fornece uma copia completa
+numeros2=[1,2,3,4,5]
+novosNumeros2=numeros2.copy() # shallow copy/copia rasa
+numeros2[0]=20
+print(novosNumeros2) # [1, 2, 3, 4, 5]
+
+
+print('-------------------------------')
+
+numeros3=[1,2,3,4,5]
+novosNumeros3=[n for n in numeros3] # list comprehension/gera uma deep copy, copias independentes  mesmo que os dados internos sejam mutaveis 
+numeros3[0]=20
+print(novosNumeros3) # [1, 2, 3, 4, 5]
+
+
+# o metodo acima é identico ao de baixo, ambos geram uma deep copy:
+
+
+numeros4=[1,2,3,4,5]
+novosNumeros4=[]
+for numero in numeros4:
+    novosNumeros4.append(numero)
+print(novosNumeros4) # [1, 2, 3, 4, 5]
