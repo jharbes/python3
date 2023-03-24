e406112b1c26dc7131d3686277908a51c508c543
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-24 11:04:43 -0300

    Update 142-dictionaryComprehention-setComprehention.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/142-dictionaryComprehention-setComprehention.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/142-dictionaryComprehention-setComprehention.py
@@ -8,10 +8,12 @@ produto = {
 dc = {
     chave: valor.upper()
     if isinstance(valor, str) else valor
-    for chave, valor
-    in produto.items()
+    for chave, valor  in produto.items()
     if chave != 'categoria'
 }
+print(dc) # {'nome': 'CANETA AZUL', 'preco': 2.5}
+
+print('-------------------------------')
 
 lista = [
     ('a', 'valor a'),
@@ -21,7 +23,16 @@ lista = [
 dc = {
     chave: valor
     for chave, valor in lista
+    # for (chave,valor) in lista # igual ao de cima, mostrando que chave, valor representa uma tupla
 }
+print(dc) # {'a': 'valor a', 'b': 'valor a'}
+
+print('-------------------------------')
+
+# set - nao garante a ordem
+# criacao de set com chaves
+s5={1,2,3}
+print(type(s5))
 
 s1 = {2 ** i for i in range(10)}
-print(s1)
+print(s1) # {32, 1, 2, 64, 4, 128, 256, 512, 8, 16}
