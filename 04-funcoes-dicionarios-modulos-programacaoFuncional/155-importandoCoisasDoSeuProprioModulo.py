f1d7ae07785adef0c969bd6b051a77d9d5d44ec8
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-31 14:50:56 -0300

    importando coisas do seu modulo fim

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/155-importandoCoisasDoSeuProprioModulo.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/155-importandoCoisasDoSeuProprioModulo.py
@@ -1,12 +1,14 @@
 # nos caminhos de sys.path
 
 import moduloTeste
-from moduloTeste import soma, variavel_modulo
+from moduloTeste import soma, variavelModulo
 
 print('Este módulo se chama', __name__)
 # print('Este módulo se chama', __name__)
-print(moduloTeste.variavel_modulo)
-print(variavel_modulo)
+
+print(moduloTeste.variavelModulo) # funciona dessa maneira com a importacao implicita de todo o moduloTeste
+
+print(variavelModulo) # funciona com apenas esse nome porque foi feita a importacao explicita dessa variavel acima
 print(soma(2, 3))
 print(moduloTeste.soma(2, 3))
 
@@ -14,6 +16,3 @@ print(moduloTeste.soma(2, 3))
 print('Este módulo se chama', __name__)
 variavel_modulo = 'Luiz'
 
-
-def soma(x, y):
-    return x + y
--- 04-funcoes-dicionarios-modulos-programacaoFuncional/moduloTeste.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/moduloTeste.py
@@ -1,1 +1,6 @@
 print('Este módulo se chama', __name__)
+
+variavelModulo='Jorge'
+
+def soma(x, y):
+    return x + y
