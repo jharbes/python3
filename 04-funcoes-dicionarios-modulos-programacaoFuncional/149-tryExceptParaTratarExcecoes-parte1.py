c364752f21e44303cb195267a01d8d84cbc065a9
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-31 10:08:02 -0300

    Update 149-tryExceptParaTratarExcecoes-parte1.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/149-tryExceptParaTratarExcecoes-parte1.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/149-tryExceptParaTratarExcecoes-parte1.py
@@ -3,6 +3,8 @@
 # b = 0
 # c = a / b
 
+# cada tipo de except trata apenas o tipo de erro respectivo, excetuando-se Exception que trata todos, caso o except nao tenha tipo tambem trata todos os tipos de erro
+
 try:
     a = 18
     b = 0
@@ -14,9 +16,9 @@ except ZeroDivisionError:
     print('Dividiu por zero.')
 except NameError:
     print('Nome b não está definido')
-except (TypeError, IndexError):
+except (TypeError, IndexError): # tratando dois erros em apenas uma linha (é possivel)
     print('TypeError + IndexError')
-except Exception:
+except Exception: # tratamento de erro generico (equivalente a ter so o except)
     print('ERRO DESCONHECIDO.')
 
 print('CONTINUAR')
