72a774e4486cd4df8e3c1008cab09f451a8b798c
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-31 10:42:31 -0300

    Update 151-tryExceptElseFinally-builtInExceptions.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/151-tryExceptElseFinally-builtInExceptions.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/151-tryExceptElseFinally-builtInExceptions.py
@@ -1,18 +1,24 @@
 # try, except, else e finally
 # https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
 
+# o finally sempre é executado, com erro ou sem erro no try
+
 try:
     print('ABRIR ARQUIVO')
     8/0
+    # print(c)
 except ZeroDivisionError as e:
     print(e.__class__.__name__)
     print(e)
     print('DIVIDIU ZERO')
 except IndexError as error:
     print('IndexError')
+    print(error)
 except (NameError, ImportError):
     print('NameError, ImportError')
+# o else so executa se o codigo nao der erros
 else:
     print('Não deu erro')
+# o finally sempre é executado, com erro ou sem erro no try
 finally:
     print('FECHAR ARQUIVO')
