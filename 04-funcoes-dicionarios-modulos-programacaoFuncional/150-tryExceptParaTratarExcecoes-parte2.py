3c3873fe36411cf711ab2dba87d8c77c295e65f4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-31 10:26:17 -0300

    Update 150-tryExceptParaTratarExcecoes-parte2.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/150-tryExceptParaTratarExcecoes-parte2.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/150-tryExceptParaTratarExcecoes-parte2.py
@@ -7,14 +7,15 @@ try:
     a = 18
     b = 0
     # print(b[0])
-    # print('Linha 1'[1000])
+    print('Linha 1'[1000])
     c = a / b
     print('Linha 2')
 except ZeroDivisionError as e:
-    print(e.__class__.__name__)
-    print(e)
-except NameError:
+    print(e.__class__.__name__) # ZeroDivisionError
+    print(e) # division by zero
+except NameError as e:
     print('Nome b não está definido')
+    print(e) # name 'b' is not defined
 except (TypeError, IndexError) as error:
     print('TypeError + IndexError')
     print('MSG:', error)
