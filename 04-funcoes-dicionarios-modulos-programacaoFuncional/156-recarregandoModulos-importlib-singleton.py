7710ab077c9b1d549b7e1dfd06f6bcfa3738c041
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-03 09:28:03 -0300

    Update 156-recarregandoModulos-importlib-singleton.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/156-recarregandoModulos-importlib-singleton.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/156-recarregandoModulos-importlib-singleton.py
@@ -1,11 +1,14 @@
-import importlib
+import importlib # biblioteca utilizada para importar modulos do python
 
 import moduloTeste156
 
 print(moduloTeste156.variavel)
 
 for i in range(10):
-    importlib.reload(moduloTeste156)
+    import moduloTeste156 # observe que ele nao reimprime os dados, a importacao eh singleton, ou seja, so pode ter uma instancia por modulo (ele nao importa de novo por questao de eficiencia)
+
+    importlib.reload(moduloTeste156) # comando para recarregar o modulo (o import puro nao funciona para isso se executado novamente)
+
     print(i)
 
 print('Fim')
