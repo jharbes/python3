c55f7b3ead2498ad64d1c25ef378a8745164708d
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-03 09:33:24 -0300

    introducao aos packages em python inicio

---
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/157-introducaoAosPackagesOuPacotesEmPython.py
@@ -0,0 +1,14 @@
+from sys import path
+
+import moduloTestePackage.moduloTeste157
+from moduloTestePackage import moduloTeste157
+from moduloTestePackage.moduloTeste157 import *
+
+# from aula99_package.modulo import soma_do_modulo
+
+# print(*path, sep='\n')
+print(soma_do_modulo(1, 2))
+print(moduloTestePackage.moduloTeste157.soma_do_modulo(1, 2))
+print(moduloTeste157.soma_do_modulo(1, 2))
+print(variavel)
+print(nova_variavel)
