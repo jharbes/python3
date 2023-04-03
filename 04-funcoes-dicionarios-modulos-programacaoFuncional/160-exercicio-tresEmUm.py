9650d3f4c664a8779701571f4f5fce17915d2830
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-03 11:23:18 -0300

    Update 160-exercicio-tresEmUm.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/160-exercicio-tresEmUm.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/160-exercicio-tresEmUm.py
@@ -14,18 +14,43 @@ produtos = [
 novos_produtos=[{**produto, 'preco': produto['preco']*1.1} for produto in produtos]
 
 print(novos_produtos) # [{'nome': 'Produto 5', 'preco': 11.0}, {'nome': 'Produto 1', 'preco': 24.552000000000003}, {'nome': 'Produto 3', 'preco': 11.121}, {'nome': 'Produto 2', 'preco': 116.45700000000001}, {'nome': 'Produto 4', 'preco': 76.89000000000001}]
+novos_produtos[0]['preco']=20
 
+print('----------------------------------------')
+
+print(novos_produtos)
+
+print('----------------------------------------')
 print(produtos) # [{'nome': 'Produto 5', 'preco': 10.0}, {'nome': 'Produto 1', 'preco': 22.32}, {'nome': 'Produto 3', 'preco': 10.11}, {'nome': 'Produto 2', 'preco': 105.87}, {'nome': 'Produto 4', 'preco': 69.9}]
+print('----------------------------------------')
 
 
+print('\n----------------------------------------\n')
 
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
+import copy
+
+produtos_ordenados_por_nome=copy.deepcopy(sorted(produtos,key=lambda produto: produto['nome'], reverse=True))
+
+print(produtos_ordenados_por_nome)
+
+print('----------------------------------------')
+
+produtos_ordenados_por_nome[0]['preco']=20
+print(produtos_ordenados_por_nome)
+print('----------------------------------------')
+print(produtos)
 
 
+print('\n----------------------------------------\n')
 
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+produtos_ordenados_por_preco=copy.deepcopy(sorted(produtos,key=lambda produto: produto['preco']))
+
+print(produtos_ordenados_por_preco)
