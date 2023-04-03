43b500b4fa298af2c8e5fce5437e1cabac164cd1
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-03 09:53:23 -0300

    introducao aos packages em python fim

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/157-introducaoAosPackagesOuPacotesEmPython.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/157-introducaoAosPackagesOuPacotesEmPython.py
@@ -1,12 +1,15 @@
 from sys import path
 
+import moduloTestePackage # importar o package apenas sozinho nao fara com que ele importe os modulos internos do package
+
 import moduloTestePackage.moduloTeste157
 from moduloTestePackage import moduloTeste157
 from moduloTestePackage.moduloTeste157 import *
 
-# from aula99_package.modulo import soma_do_modulo
+from moduloTestePackage.moduloTeste157 import soma_do_modulo
 
-# print(*path, sep='\n')
+print(__name__)
+print(*path, sep='\n')
 print(soma_do_modulo(1, 2))
 print(moduloTestePackage.moduloTeste157.soma_do_modulo(1, 2))
 print(moduloTeste157.soma_do_modulo(1, 2))
--- 04-funcoes-dicionarios-modulos-programacaoFuncional/moduloTestePackage/moduloTeste157.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/moduloTestePackage/moduloTeste157.py
@@ -1,8 +1,11 @@
-# __all__ = [
-#     'variavel',
-#     'soma_do_modulo',
-#     'nova_variavel',
-# ]
+# quando a importacao for por meio do comando:
+# from moduloTestePackage.moduloTeste157 import *
+# poderemos usar o comando abaixo pra dizer o que sera efetivamente importado (__all__ = *), na ausencia dela o mesmo comando importara tudo que estiver no codigo do modulo
+__all__ = [
+    'variavel',
+    'soma_do_modulo',
+    'nova_variavel',
+]
 
 variavel = 'Alguma coisa'
 
