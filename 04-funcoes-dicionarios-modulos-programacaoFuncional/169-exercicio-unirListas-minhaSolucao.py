f14f3cc04f29b5f989681a19d74d937e30f9b5db
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-04 14:56:47 -0300

    unir listas - minha solucao

---
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/169-exercicio-unirListas-minhaSolucao.py
@@ -0,0 +1,34 @@
+# Exercício - Unir listas
+
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+import copy
+
+listaCidades=['Salvador', 'Ubatuba', 'Belo Horizonte']
+listaEstados=['BA', 'SP', 'MG', 'RJ']
+
+def funcaoZipper(lista1,lista2):
+    if len(lista1)<len(lista2):
+        menor=copy.deepcopy(lista1)
+        maior=copy.deepcopy(lista2)
+    else:
+        menor=copy.deepcopy(lista2)
+        maior=copy.deepcopy(lista1)
+
+    listaZipper=copy.deepcopy(menor)
+
+
+    for index,item in enumerate(listaZipper):
+        listaZipper[index]=item,maior[index]
+
+    return listaZipper
+
+funcaoZipper(listaCidades,listaEstados)
