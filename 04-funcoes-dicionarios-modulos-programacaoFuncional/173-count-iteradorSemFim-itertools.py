821e63b2dfc88c0bbd622beaca634cb691d5fd6e
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-06 13:30:09 -0300

    Update 173-count-iteradorSemFim-itertools.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/173-count-iteradorSemFim-itertools.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/173-count-iteradorSemFim-itertools.py
@@ -1,13 +1,16 @@
 # count é um iterador sem fim (itertools)
 from itertools import count
 
-c1 = count(step=8, start=8)
-r1 = range(8, 100, 8)
+c1 = count(step=8, start=8) # começa em 8 e anda de 8 em 8 mas nao tem fim
+
+r1 = range(8, 100, 8) # comeca em 8, anda de 8 em 8 e termina no 100
 
 print('c1', hasattr(c1, '__iter__'))
 print('c1', hasattr(c1, '__next__'))
 print('r1', hasattr(r1, '__iter__'))
 print('r1', hasattr(r1, '__next__'))
+
+print('---------------------------')
 
 print('count')
 for i in c1:
