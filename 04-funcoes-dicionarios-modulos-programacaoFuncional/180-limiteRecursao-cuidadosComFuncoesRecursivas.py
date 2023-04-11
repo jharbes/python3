c132e13bd52f8173dc2a448660e9dfc12c101176
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-11 14:00:29 -0300

    Update 180-limiteRecursao-cuidadosComFuncoesRecursivas.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/180-limiteRecursao-cuidadosComFuncoesRecursivas.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/180-limiteRecursao-cuidadosComFuncoesRecursivas.py
@@ -2,11 +2,12 @@
 # - fatorial - n! = 5! = 5 * 4 * 3 * 2 * 1 = 120
 # https://brasilescola.uol.com.br/matematica/fatorial.htm
 
+import sys
+sys.setrecursionlimit(1004) # altera o limite de recursao do python
+
 def recursiva(inicio=0, fim=4):
-# import sys
 
     print(inicio, fim)
-# sys.setrecursionlimit(1004)
 
     # Caso base
     if inicio >= fim:
@@ -16,22 +17,12 @@ def recursiva(inicio=0, fim=4):
     # contar até chegar ao final
     inicio += 1
     return recursiva(inicio, fim)
-# def recursiva(inicio=0, fim=4):
 
-#     print(inicio, fim)
 
 print(recursiva())
-#     # Caso base
-#     if inicio >= fim:
-#         return fim
 
-#     # Caso recursivo
-#     # contar até chegar ao final
-#     inicio += 1
-#     return recursiva(inicio, fim)
+# print(recursiva(0, 1000))
 
-
-# print(recursiva(0, 1001))
 
 def factorial(n):
     if n <= 1:
