437a7054db05e5441a88b27ce3498b3d5b5d18cd
Jorge Harbes <jharbes@hotmail.com>
2023-04-15 14:57:47 -0300

    Create 184-pip-instalandoPacotesEBibliotecas.py

---
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/184-pip-instalandoPacotesEBibliotecas.py
@@ -0,0 +1,10 @@
+# pip - instalando pacotes e bibliotecas
+# Instalar última versão:
+# pip install nome_pacote
+# Instalar versão precisa
+# (tem outras formas também não mencionadas)
+# pip install nome_pacote==0.0.0
+# Desinstalar pacote
+# pip uninstall nome_pacote
+# Congelar (ver pacotes)
+# pip freeze
