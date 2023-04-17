cdab908576183d79432ddab816142be07ebe3959
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-17 14:20:36 -0300

    Update 195-exercicio-salvandoListaTarefasEmJson.py

--- 04-funcoes-dicionarios-modulos-programacaoFuncional/195-exercicio-salvandoListaTarefasEmJson.py
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/195-exercicio-salvandoListaTarefasEmJson.py
@@ -9,3 +9,40 @@
 
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
+
+def printarTarefas():
+    print('TAREFAS:')
+    if len(todoList)<1:
+        print('Não há tarefas para serem listadas!')
+    else:
+        for task in todoList:
+            print(task)
+    print()
+
+todoList=[]
+refazerList=[]
+
+
+while True:
+    print('Comandos: listar, desfazer, refazer, fim')
+    tarefa=input('Digite uma tarefa ou comando: ')
+    print()
+    if tarefa=='listar':
+        printarTarefas()
+    elif tarefa=='desfazer':
+        if len(todoList)<1:
+            print('Não há tarefa para desfazer!\n')
+        else:
+            refazerList.append(todoList.pop())
+            printarTarefas()
+    elif tarefa=='refazer':
+        if len(refazerList)<1:
+            print('Não há tarefa para refazer!\n')
+        else:
+            todoList.append(refazerList.pop())
+            printarTarefas()
+    elif tarefa=='fim':
+        break
+    else:
+        todoList.append(tarefa)
+        printarTarefas()
