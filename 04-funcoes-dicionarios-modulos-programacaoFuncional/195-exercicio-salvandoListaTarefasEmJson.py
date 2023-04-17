f22512f97483964a3e9c6fddae2b39e1c6a115bc
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-17 14:02:22 -0300

    Create 195-exercicio-salvandoListaTarefasEmJson.py

---
+++ 04-funcoes-dicionarios-modulos-programacaoFuncional/195-exercicio-salvandoListaTarefasEmJson.py
@@ -0,0 +1,11 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
