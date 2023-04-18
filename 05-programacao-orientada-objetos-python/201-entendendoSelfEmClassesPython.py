c73dafa26831489931c1a5049ff74b58dad64a5b
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-18 10:31:34 -0300

    Update 201-entendendoSelfEmClassesPython.py

--- 05-programacao-orientada-objetos-python/201-entendendoSelfEmClassesPython.py
+++ 05-programacao-orientada-objetos-python/201-entendendoSelfEmClassesPython.py
@@ -1,8 +1,14 @@
 # Entendendo self em classes Python
 # Classe - Molde (geralmente sem dados)
+
 # Instância da class (objeto) - Tem os dados
+
 # Uma classe pode gerar várias instâncias.
+
 # Na classe o self é a própria instância.
+
+# o nome 'self' é apenas uma convencao, poderia ser usado qualquer outro nome em seu lugar
+
 class Carro:
     def __init__(self, nome):
         self.nome = nome
@@ -12,12 +18,12 @@ class Carro:
 
 
 fusca = Carro('Fusca')
-fusca.acelerar()
-Carro.acelerar(fusca)
+fusca.acelerar() # Fusca está acelerando...
+Carro.acelerar(fusca) # Fusca está acelerando...
 # print(fusca.nome)
 # fusca.acelerar()
 
 celta = Carro(nome='Celta')
-celta.acelerar()
-Carro.acelerar(celta)
+celta.acelerar() # Celta está acelerando...
+Carro.acelerar(celta) # Celta está acelerando...
 # print(celta.nome)
