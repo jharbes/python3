f1c10a3fdd596f6835d0535548a2334d2de4baa4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-18 13:08:14 -0300

    salve sua classe em json

--- 05-programacao-orientada-objetos-python/206-exercicio-salveSuaClasseEmJson.py
+++ 05-programacao-orientada-objetos-python/206-exercicio-salveSuaClasseEmJson.py
@@ -4,3 +4,35 @@
 # e depois crie novamente as instâncias
 # da classe com os dados salvos
 # Faça em arquivos separados.
+
+import json
+
+cadastro={}
+
+caminhoArquivoJson='.\\05-programacao-orientada-objetos-python\\206-json-arquivoAuxiliar.json'
+
+class Pessoa:
+    def __init__(self,nome,idade,email,profissao) -> None:
+        self.nome=nome
+        self.idade=idade
+        self.email=email
+        self.profissao=profissao
+
+
+p1=Pessoa('Jorge Nami Harbes',39,'jharbes@hotmail.com','Software Developer')
+cadastro['p1']=f'{p1.__dict__}'
+p2=Pessoa('Carolina Ferreira Alcântara',28,'carolalcantara94@gmail.com','Professora de Educação Física')
+cadastro['p2']=f'{p2.__dict__}'
+p3=Pessoa('Maria Ferreira Barreto',89,'maria@barreto.com','Aposentada')
+cadastro['p3']=f'{p3.__dict__}'
+
+
+
+
+with open(caminhoArquivoJson, 'w', encoding='utf8') as arquivo:
+    json.dump(
+        cadastro,
+        arquivo,
+        ensure_ascii=False,
+        indent=2,
+    )
