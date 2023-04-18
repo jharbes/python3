9a67cd10f98b4bec6bde776df5640507626ddb81
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-18 09:35:18 -0300

    Update 198-class-classesMoldesParaCriarObjetos.py

--- 05-programacao-orientada-objetos-python/198-class-classesMoldesParaCriarObjetos.py
+++ 05-programacao-orientada-objetos-python/198-class-classesMoldesParaCriarObjetos.py
@@ -3,25 +3,32 @@
 # podem ter seus próprios atributos e métodos.
 # Os objetos gerados pela classe podem usar seus dados
 # internos para realizar várias ações.
+
 # Por convenção, usamos PascalCase para nomes de
 # classes.
+
 # string = 'Luiz'  # str
 # print(string.upper())
 # print(isinstance(string, str))
+
 class Pessoa:
     ...
 
 
-p1 = Pessoa('Luiz', 'Otávio')
+p1 = Pessoa()
 p1.nome = 'Luiz'
 p1.sobrenome = 'Otávio'
 
-p2 = Pessoa('Maria', 'Joana')
+p2 = Pessoa()
 p2.nome = 'Maria'
 p2.sobrenome = 'Joana'
 
-print(p1.nome)
-print(p1.sobrenome)
+print(p1) # <__main__.Pessoa object at 0x0000020AF5BE0C10>
+print(p1.nome) # Luiz
+print(p1.sobrenome) # Otávio
 
-print(p2.nome)
-print(p2.sobrenome)
+print()
+
+print(p2) # <__main__.Pessoa object at 0x0000020AF5BE0970>
+print(p2.nome) # Maria
+print(p2.sobrenome) # Joana
