cb3fc109bbfa7f5c1c61c7d71fc1c7e93f3acb49
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-19 09:06:33 -0300

    Update 206b-exercicio-carregandoSuaClasseEmJson-minhaSolucao.py

--- 05-programacao-orientada-objetos-python/206b-exercicio-carregandoSuaClasseEmJson-minhaSolucao.py
+++ 05-programacao-orientada-objetos-python/206b-exercicio-carregandoSuaClasseEmJson-minhaSolucao.py
@@ -19,23 +19,33 @@ class Pessoa:
         self.profissao=profissao
 
 
-
 listaPessoas=[]
+
 for pessoa in arquivoJson['pessoas']:
-    pessoa=pessoa.replace('{','').replace('}','').replace("'",'').replace(': ',':').strip()
-    listaPessoa=pessoa.split(',')
-    for item in listaPessoa:
-        if 'nome' in item:
-            nome=item[item.find(':')+1:]
-        elif 'idade' in item:
-            idade=item[item.find(':')+1:]
-        elif 'email' in item:
-            email=item[item.find(':')+1:]
-        elif 'profissao' in item:
-            profissao=item[item.find(':')+1:]
-    listaPessoas.append(Pessoa(nome,idade,email,profissao))
+    listaPessoas.append(Pessoa(pessoa['nome'],pessoa['idade'],pessoa['email'],pessoa['profissao']))
 
 
 for pessoa in listaPessoas:
     print(pessoa.__dict__)
+    print()
+
+
+# listaPessoas=[]
+# for pessoa in arquivoJson['pessoas']:
+#     pessoa=pessoa.replace('{','').replace('}','').replace("'",'').replace(': ',':').strip()
+#     listaPessoa=pessoa.split(',')
+#     for item in listaPessoa:
+#         if 'nome' in item:
+#             nome=item[item.find(':')+1:]
+#         elif 'idade' in item:
+#             idade=item[item.find(':')+1:]
+#         elif 'email' in item:
+#             email=item[item.find(':')+1:]
+#         elif 'profissao' in item:
+#             profissao=item[item.find(':')+1:]
+#     listaPessoas.append(Pessoa(nome,idade,email,profissao))
+
+
+# for pessoa in listaPessoas:
+#     print(pessoa.__dict__)
 
