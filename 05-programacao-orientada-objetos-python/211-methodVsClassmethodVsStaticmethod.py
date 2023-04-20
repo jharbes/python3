8f7259893bb08a9dfa69918b65af25ad499975bd
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-20 16:57:11 -0300

    Update 211-methodVsClassmethodVsStaticmethod.py

--- 05-programacao-orientada-objetos-python/211-methodVsClassmethodVsStaticmethod.py
+++ 05-programacao-orientada-objetos-python/211-methodVsClassmethodVsStaticmethod.py
@@ -2,12 +2,14 @@
 # method - self, método de instância
 # @classmethod - cls, método de classe
 # @staticmethod - método estático (❌self, ❌cls)
+
 class Connection:
     def __init__(self, host='localhost'):
         self.host = host
         self.user = None
         self.password = None
 
+    # setter
     def set_user(self, user):
         self.user = user
 
@@ -30,10 +32,17 @@ def connection_log(msg):
     print('LOG:', msg)
 
 
-# c1 = Connection()
-c1 = Connection.create_with_auth('luiz', '1234')
-# c1.set_user('luiz')
-# c1.set_password('123')
-print(Connection.log('Essa é a mensagem de log'))
-print(c1.user)
-print(c1.password)
+c2 = Connection()
+
+c1 = Connection.create_with_auth('luiz','1234')
+
+c2.set_user('cachorro')
+ 
+c2.set_password('123')
+
+print(Connection.log('Essa é a mensagem de log')) # LOG: Essa é a mensagem de log None 
+
+print(c1.user) # luiz
+
+print(c1.password) # 1234
+print(c2.__dict__) # {'host': 'localhost', 'user': 'cachorro', 'password': '123'}
