ee29933ceee5a3c6aeca59d5034e4d80e3b2dcbd
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-03 12:35:17 -0300

    Update 215-relacaoEntreClasses-associacaoAgregacaoComposicao.py

--- 05-programacao-orientada-objetos-python/215-relacaoEntreClasses-associacaoAgregacaoComposicao.py
+++ 05-programacao-orientada-objetos-python/215-relacaoEntreClasses-associacaoAgregacaoComposicao.py
@@ -1,12 +1,15 @@
 # Relações entre classes: associação, agregação e composição
+
 # Associação é um tipo de relação onde os objetos
 # estão ligados dentro do sistema.
 # Essa é a relação mais comum entre objetos e tem subconjuntos
 # como agregação e composição (que veremos depois).
+
 # Geralmente, temos uma associação quando um objeto tem
 # um atributo que referencia outro objeto.
 # A associação não especifica como um objeto controla
 # o ciclo de vida de outro objeto.
+
 class Escritor:
     def __init__(self, nome) -> None:
         self.nome = nome
@@ -34,6 +37,6 @@ caneta = FerramentaDeEscrever('Caneta Bic')
 maquina_de_escrever = FerramentaDeEscrever('Máquina')
 escritor.ferramenta = maquina_de_escrever
 
-print(caneta.escrever())
-print(maquina_de_escrever.escrever())
-print(escritor.ferramenta.escrever())
+print(caneta.escrever()) # Caneta Bic está escrevendo
+print(maquina_de_escrever.escrever()) # Máquina está escrevendo
+print(escritor.ferramenta.escrever()) # Máquina está escrevendo
