ca4bc22b19bb4eb3ec64bba3acce257aa3d447c3
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-04 16:32:11 -0300

    Update 218-exercicio-classesRelacoes-minhaSolucao.py

--- 05-programacao-orientada-objetos-python/218-exercicio-classesRelacoes-minhaSolucao.py
+++ 05-programacao-orientada-objetos-python/218-exercicio-classesRelacoes-minhaSolucao.py
@@ -8,3 +8,50 @@
 # Obs.: Um fabricante pode fabricar vários carros
 # Exiba o nome do carro, motor e fabricante na tela
 
+class Carro:
+    def __init__(self,nome) -> None:
+        self._nome=nome
+        self._motor=None
+        self._fabricante=None
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self,nome):
+        self._nome=nome
+
+    @property
+    def motor(self):
+        return self._motor
+    
+    @motor.setter
+    def motor(self,motor):
+        self._motor=motor
+
+    @property
+    def fabricante(self):
+        return self._fabricante
+    
+    @fabricante.setter
+    def fabricante(self,fabricante):
+        self._fabricante=fabricante
+
+
+class Motor:
+    def __init__(self,tipo) -> None:
+        self.tipo=tipo
+
+
+class Fabricante:
+    def __init__(self,nome) -> None:
+        self.nome=nome
+
+
+carro=Carro('Golf GTI')
+carro.motor='1.5 turbo'
+carro.fabricante='Volkswagen'
+
+print(vars(carro))
+print(carro.__dict__)
