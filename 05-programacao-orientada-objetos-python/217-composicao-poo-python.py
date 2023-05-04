da327bf3461807120d8a5d3da18eade4ed8dcc8a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-04 15:19:02 -0300

    Update 217-composicao-poo-python.py

--- 05-programacao-orientada-objetos-python/217-composicao-poo-python.py
+++ 05-programacao-orientada-objetos-python/217-composicao-poo-python.py
@@ -1,8 +1,10 @@
 # Relações entre classes: associação, agregação e composição
+
 # Composição é uma especialização da agregação.
 # Mas nela, quando o objeto "pai" for apagado, todas
 # as referências dos objetos filhos também são
 # apagadas.
+
 class Cliente:
     def __init__(self, nome):
         self.nome = nome
