1a9165f624da45c46d80297f967eb8466824b7d5
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-05 15:25:35 -0300

    Update 220-herancaSimples-python-poo.py

--- 05-programacao-orientada-objetos-python/220-herancaSimples-python-poo.py
+++ 05-programacao-orientada-objetos-python/220-herancaSimples-python-poo.py
@@ -1,6 +1,10 @@
 # Herança simples - Relações entre classes
-# Associação - usa, Agregação - tem
-# Composição - É dono de, Herança - É um
+
+# Associação - usa outro objeto
+# Agregação - tem outro objeto
+# Composição - É dono de outro objeto
+# Herança - É um subtipo do objeto pai
+
 #
 # Herança vs Composição
 #
@@ -8,6 +12,7 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+
 class Pessoa:
     cpf = '1234'
 
@@ -15,6 +20,7 @@ class Pessoa:
         self.nome = nome
         self.sobrenome = sobrenome
 
+    # self.__class__.__name__ imprime o nome da classe
     def falar_nome_classe(self):
         print('Classe PESSOA')
         print(self.nome, self.sobrenome, self.__class__.__name__)
@@ -32,8 +38,52 @@ class Aluno(Pessoa):
 
 
 c1 = Cliente('Luiz', 'Otávio')
-c1.falar_nome_classe()
+c1.falar_nome_classe() 
+# EITA, nem saí da classe CLIENTE
+# Luiz Otávio Cliente
+
 a1 = Aluno('Maria', 'Helena')
 a1.falar_nome_classe()
-print(a1.cpf)
-# help(Cliente)
+# Classe PESSOA
+# Maria Helena Aluno
+
+print(a1.cpf) # cpf aluno
+print(a1.__dict__) # {'nome': 'Maria', 'sobrenome': 'Helena'}
+
+help(Cliente)
+"""
+Help on class Cliente in module __main__:                       
+
+class Cliente(Pessoa)
+ |  Cliente(nome, sobrenome)
+ |  
+ |  Method resolution order:  ## ordem em que o python procura as informacoes nas classes
+ |      Cliente
+ |      Pessoa
+ |      builtins.object
+ |  
+ |  Methods defined here:
+ |  
+ |  falar_nome_classe(self)
+ |  
+ |  ----------------------------------------------------------------------
+ |  Methods inherited from Pessoa:
+ |  
+ |  __init__(self, nome, sobrenome)
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |  
+ |  ----------------------------------------------------------------------
+ |  Data descriptors inherited from Pessoa:
+ |  
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |  
+ |  __weakref__
+ |      list of weak references to the object (if defined)      
+ |
+ |  ----------------------------------------------------------------------
+ |  Data and other attributes inherited from Pessoa:
+ |
+ |  cpf = '1234'
+
+"""
