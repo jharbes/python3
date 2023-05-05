b5e1a33f5450c886346b1719e5731aef4c0f1192
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-05 16:39:56 -0300

    Update 223-teoria-herancaMultipla-poo-python.py

--- 05-programacao-orientada-objetos-python/223-teoria-herancaMultipla-poo-python.py
+++ 05-programacao-orientada-objetos-python/223-teoria-herancaMultipla-poo-python.py
@@ -1,8 +1,10 @@
 # Herança Múltipla - Python Orientado a Objetos
 # Quer dizer que no Python, uma classe pode estender
 # várias outras classes.
+
 # Herança simples:
 # Animal -> Mamifero -> Humano -> Pessoa -> Cliente
+
 # Herança múltipla e mixins
 # Log -> FileLog
 # Animal -> Mamifero -> Humano -> Pessoa -> Cliente
