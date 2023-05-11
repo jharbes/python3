1d952aa7c2f4c9cb5d86fd233c6e2710a83a7c26
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-11 13:38:38 -0300

    Update log.py

--- 05-programacao-orientada-objetos-python/225-mixins-abstracao-tudoAteAqui-parte1.py/log.py
+++ 05-programacao-orientada-objetos-python/225-mixins-abstracao-tudoAteAqui-parte1.py/log.py
@@ -8,7 +8,7 @@ class LogFileMixin(Log):
     def log(self, msg):
         print(msg)
 
-
+# nesse caso só irá executar caso a execucao seja feita diretamente no modulo log.py, caso seja em algum outro modulo que importa log ele nao efetuara os comandos abaixo como rodar o main.py
 if __name__ == '__main__':
     l = LogFileMixin()
     l.log('qualquer coisa')
