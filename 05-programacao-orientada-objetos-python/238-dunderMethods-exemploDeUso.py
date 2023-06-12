47f6b9b0ec58643f7005dfe039f0794ac2043a21
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-12 15:14:50 -0300

    Update 238-dunderMethods-exemploDeUso.py

--- 05-programacao-orientada-objetos-python/238-dunderMethods-exemploDeUso.py
+++ 05-programacao-orientada-objetos-python/238-dunderMethods-exemploDeUso.py
@@ -19,6 +19,7 @@ class Ponto:
         self.x = x
         self.y = y
 
+    # representacao entre desenvolvedores, pois alem de ter os dados do objeto tambem possui informacoes da classe
     def __repr__(self):
         class_name = type(self).__name__
         return f'{class_name}(x={self.x!r}, y={self.y!r})'
@@ -38,6 +39,13 @@ if __name__ == '__main__':
     p1 = Ponto(4, 2)  # 6
     p2 = Ponto(6, 4)  # 10
     p3 = p1 + p2
-    print(p3)
-    print('P1 é maior que p2', p1 > p2)
-    print('P2 é maior que p1', p2 > p1)
+
+
+    # observe que o dunder __add__ manipulou a expressao de soma para que ela retorne um valor da maneira que foi conveniente que fosse feita para o desenvolvedor, sem essa manipulacao nao haveria sequer um retorno
+    print(p3) # Ponto(x=10, y=6)
+
+
+    # idem ao anterior, o dunder __gt__ manipulou a expressao > de maneira que a expressao retornasse o valor 'MAIOR' de acordo com o interessante para o desenvolvedor, de maneira diferente nao haveria retorno
+    print('P1 é maior que p2', p1 > p2) # P1 é maior que p2 False
+
+    print('P2 é maior que p1', p2 > p1) # P2 é maior que p1 True
