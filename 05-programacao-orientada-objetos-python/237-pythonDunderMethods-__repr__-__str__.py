5a117daa2ff91ba84fe6df22bd160c80995f0ec7
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-12 14:56:28 -0300

    Update 237-pythonDunderMethods-__repr__-__str__.py

--- 05-programacao-orientada-objetos-python/237-pythonDunderMethods-__repr__-__str__.py
+++ 05-programacao-orientada-objetos-python/237-pythonDunderMethods-__repr__-__str__.py
@@ -28,13 +28,22 @@ class Ponto:
         return f'({self.x}, {self.y})'
 
     def __repr__(self):
+        # as duas expressoes abaixo para class_name retornarao o nome da classe:
         # class_name = self.__class__.__name__
         class_name = type(self).__name__
+
         return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
 
 
 p1 = Ponto(1, 2)
 p2 = Ponto(978, 876)
-print(p1)
-print(repr(p2))
-print(f'{p2!r}')
+
+print(p1) # (1, 2)
+
+print(repr(p2)) # Ponto(x=978, y=876, z='String')
+
+
+# em f strings {p2} retornaria o __str__ e {p2!r} retornaria o __repr__
+print(f'{p2}') # (978, 876)
+print(f'{p2!s}') # (978, 876)
+print(f'{p2!r}') # Ponto(x=978, y=876, z='String')
