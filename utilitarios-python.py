54ca9a8f12c4fdd6e9b2ab681350ca2368dc90df
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-21 09:30:47 -0300

    Update utilitarios-python.py

--- utilitarios-python.py
+++ utilitarios-python.py
@@ -90,14 +90,38 @@ except Exception as error:
 # for em python (com indice)
 
 for indice,item in enumerate(vetorOuString):
-            print(letra)
+    print(letra)
             
             
             
 
-# pegando data e hora atual            
+# criando data e hora atual com formato editado
 from datetime import datetime
 import pytz # faz o ajuste de fuso horario
 
+fusoHorarioBrasil=pytz.timezone('Brazil/East')
+
 def dataHoraAtual():
     return datetime.now(fusoHorarioBrasil).strftime('%d/%m/%Y %H:%M:%S') # metodo strftime mostra a data/hora em modo mais amigavel e legivel
+
+
+def dataHora():
+    return datetime.now(fusoHorarioBrasil)
+
+
+
+# criando formatacao para os valores (em reais)
+import locale
+
+locale.setlocale(locale.LC_MONETARY,'pt_BR.UTF-8')
+
+def formatacaoMoeda(valor):
+    return locale.currency(valor,grouping=True)
+
+
+
+
+# formatando percentual
+def formatacaoPercentual(valor):
+    valor*=100
+    return '{}%'.format(round(valor,2))
