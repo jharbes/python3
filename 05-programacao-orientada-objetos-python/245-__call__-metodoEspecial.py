e5f1c1345b1feff7078f467bd26b7be3b69ab9d0
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-26 11:09:07 -0300

    Update 245-__call__-metodoEspecial.py

--- 05-programacao-orientada-objetos-python/245-__call__-metodoEspecial.py
+++ 05-programacao-orientada-objetos-python/245-__call__-metodoEspecial.py
@@ -2,15 +2,21 @@
 # callable é algo que pode ser executado com parênteses
 # Em classes normais, __call__ faz a instância de uma
 # classe "callable".
+
 class CallMe:
     def __init__(self, phone):
         self.phone = phone
 
+    # faz com que a instacia de cada objeto da classe acima (CallMe) seja chamavel como uma funcao
+    # cancela o erro 'object is not callable'
     def __call__(self, nome):
         print(nome, 'está chamando', self.phone)
         return 2134
 
 
 call1 = CallMe('23945876545')
-retorno = call1('Luiz Otávio')
-print(retorno)
+
+# aqui chamamos a instacia call1 passando o argumento 'Luiz Otávio'
+retorno = call1('Luiz Otávio') # Luiz Otávio está chamando 23945876545
+
+print(retorno) # 2134
