d35443fd0a6b199fcc8550156ab40464eadb27c0
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-13 12:51:54 -0300

    Update main.py

--- 05-programacao-orientada-objetos-python/253-DocString-classes-documentacao/main.py
+++ 05-programacao-orientada-objetos-python/253-DocString-classes-documentacao/main.py
@@ -1,3 +1,4 @@
 import documentando_class
 
+# observe que a documentação está sendo preenchida tanto com o type hinting (quando construimos a funcao colocando os tipos dos argumentos aceitos e tipo de saida logo no cabeçalho) quanto a documentação normal onde explicamos o funcionamento em texto livre
 help(documentando_class)
